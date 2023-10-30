808eb67d95d67785b4f11557a08ed2f43b63ef1b
Aaron Nakyenalire <nakyenalire@gmail.com>
2023-10-30 13:38:15 +0300

    first commit

---
+++ README.dm
@@ -0,0 +1,18 @@
+//localstorage
+.. no expiration date
+CRUD
+//creation
+localstorage.setItem('key', value)
+//retrieving
+localstorage.getItem('key')
+
+//remove items
+localstorage.removeItem(key)
+
+JSON.stringify(objectName)
+
+JSON.parse(string)
+
+
+//session storage
+.. expiry lasts only when browser is open
---
+++ index.js
@@ -0,0 +1,60 @@
+let myInput = document.getElementById("input");
+let myTasks = document.getElementById("tasks");
+myTasks.addEventListener("click", function (e) {
+    function savedData() {
+
+        let dataArr = [];
+        let data = localStorage.getItem("value");
+
+        if (data) {
+            dataArr = JSON.parse(data);
+        }
+
+        let savedValue = myInput.value;
+        dataArr.push(savedValue)
+        localStorage.setItem('value', JSON.stringify(dataArr));
+        console.log(dataArr)
+
+        renderitems(dataArr)
+        myInput.value = "";
+
+
+
+    }
+    savedData()
+})
+
+function renderitems(dataArr) {
+    let returnedValue = document.querySelector('.to-do-list1')
+    returnedValue.innerHTML = "";
+    for (let tasks of dataArr) {
+        let listItems = document.createElement('ol')
+        let EditBtn = document.createElement("button")
+        EditBtn.innerText = "Edit";
+        EditBtn.classList = "editBtn"
+        let deleteButton = document.createElement('button')
+        deleteButton.innerText = "Delete"
+
+        listItems.textContent = tasks;
+        listItems.appendChild(EditBtn)
+        listItems.appendChild(deleteButton)
+        returnedValue.appendChild(listItems)
+        deleteButton = document.querySelector("Delete")
+        deleteButton.style.backgroundColor = orange;
+
+        EditBtn.addEventListener("click", function (e) {
+        
+            console.log("edited")
+        })
+        deleteButton.addEventListener("click", function (e) {
+
+
+            console.log("deleted")
+        })
+
+
+
+    }
+
+}
+// function 
